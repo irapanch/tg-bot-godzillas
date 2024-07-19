27e3058706be6766fd00ee96263a31d3cfb26a5b
irapanch <117355580+irapanch@users.noreply.github.com>
2024-07-19 12:39:34 +0300

    test2

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,7 +3,7 @@ import TeleBot from "telebot";
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.on("text", (msg) => {
-  //   msg.reply.text(msg.text); // Відповідь на текстове повідомлення
-  msg.reply.text("Hello!"); // Відправка повідомлення "Hello!"
+  msg.reply.text(msg.text); // Відповідь на текстове повідомлення
+  //   msg.reply.text("Hello!"); // Відправка повідомлення "Hello!"
 });
 export default bot;

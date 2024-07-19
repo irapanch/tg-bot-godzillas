9201120cb78db8f2c7e3d56753dde215b435be28
irapanch <117355580+irapanch@users.noreply.github.com>
2024-07-19 12:19:48 +0300

    init

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,7 @@
-import TeleBot from "telebot"
+import TeleBot from "telebot";
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", msg => msg.reply.text(msg.text))
-
-export default bot
+bot.on("text", (msg) => msg.reply.text(msg.text));
+bot.on("text", () => bot.sendMessage(msg.from.id, "Hello!"));
+export default bot;

bd0dfd791cc07d9348340474f4d8e87895154a95
Iryna Panchenko <3041804428puh@gmail.com>
2024-07-19 13:26:14 +0300

    add foto

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,6 +2,8 @@ import TeleBot from "telebot";
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", (msg) => msg.reply.text(msg.text));
-
+bot.on("text", (msg) => msg.text.startWith('/')?null:msg.reply.text(msg.text));
+bot.on('/start', (msg) => {
+    msg.reply.photo('https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXA2NTAzbWpraWZ6N2JyYjIyN3dpa2s1eWM3emppcjV2am50MHkyZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/4kdhrgN4bXwmu3fBDy/giphy.gif')
+}));
 export default bot;
